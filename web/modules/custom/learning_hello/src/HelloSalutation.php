6c25d5a29073308510519ceeb4cb5ba28703b773
gabriel-dobrita-collinson <gabriel.dobrita@collinsongroup.com>
2024-07-17 12:22:06 +0300

    Service dependencies

--- web/modules/custom/learning_hello/src/HelloSalutation.php
+++ web/modules/custom/learning_hello/src/HelloSalutation.php
@@ -2,25 +2,55 @@
 
 namespace Drupal\learning_hello;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 
+/**
+ * Class HelloSalutation.
+ */
 class HelloSalutation {
+
   use StringTranslationTrait;
 
   /**
-   * Returns the salutation
+   * The config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * HelloSalutation constructor.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The config factory.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory) {
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * Returns the salutation.
+   *
+   * @return \Drupal\Core\StringTranslation\TranslatableMarkup|string
+   *   The salutation.
    */
   public function getSalutation() {
+    $config = $this->configFactory->get('learning_hello.custom_salutation');
+    $salutation = $config->get('salutation');
+
+    if ($salutation !== "" && $salutation) {
+      return $salutation;
+    }
+
     $time = new \DateTime();
-    if ((int) $time->format('G') >= 00 && (int) $time->
-      format('G') < 12) {
+    if ((int) $time->format('G') >= 0 && (int) $time->format('G') < 12) {
       return $this->t('Good morning world');
     }
-    if ((int) $time->format('G') >= 12 && (int) $time->
-      format('G') < 18) {
+    elseif ((int) $time->format('G') >= 12 && (int) $time->format('G') < 18) {
       return $this->t('Good afternoon world');
     }
-    if ((int) $time->format('G') >= 18) {
+    else {
       return $this->t('Good evening world');
     }
   }
