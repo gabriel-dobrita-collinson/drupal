07bb80e6ad284701426d1e720ac09a87018e8f11
gabriel-dobrita-collinson <gabriel.dobrita@collinsongroup.com>
2024-07-17 13:17:48 +0300

    Add custom block

---
+++ web/modules/custom/learning_hello/src/Plugin/Block/HelloSalutationBlock.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\learning_hello\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\learning_hello\HelloSalutation;
+
+/**
+ * Provides a 'Hello Salutation' Block.
+ *
+ * @Block(
+ *   id = "learning_hello_salutation_block",
+ *   admin_label = @Translation("Hello salutation"),
+ * )
+ */
+class HelloSalutationBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The salutation service.
+   *
+   * @var \Drupal\learning_hello\HelloSalutation
+   */
+  protected $salutation;
+
+  /**
+   * Constructs a HelloSalutationBlock.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\learning_hello\HelloSalutation $salutation
+   *   The salutation service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, HelloSalutation $salutation) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->salutation = $salutation;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('learning_hello.salutation')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return [
+      '#markup' => $this->salutation->getSalutation(),
+    ];
+  }
+}
